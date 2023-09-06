42d799b67f7738490354aa945c3eaa69d6f65a4b
LeonTLam <s352005@oslomet.no>
2023-09-06 12:34:35 +0200

    Add project files.

---
+++ GroupAssignment1/Models/Housing.cs
@@ -0,0 +1,11 @@
+﻿namespace GroupAssignment1.Models
+{
+    public class Housing
+    {
+        public int HousingId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Rent { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }   
+    }
+}
