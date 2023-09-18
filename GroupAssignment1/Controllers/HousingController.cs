16b7341d3cfa7daf3e35e03e2134009e6d13fcee
Leon Tongfai Lam <s352005@oslomet.no>
2023-09-18 21:48:08 +0200

    Second commit
    +libmanager
    +jquery
    +bootstrap
    +Table.cshtml
    +image(s)

--- GroupAssignment1/Controllers/HousingController.cs
+++ GroupAssignment1/Controllers/HousingController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using GroupAssignment1.Models; 
 
 namespace GroupAssignment1.Controllers
 {
     public class HousingController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Table()
         {
-            return View();
+            var housings = new List<Housing>();
+            var housing1 = new Housing();
+
+            housing1.HousingId = 1;
+            housing1.Name = "Hus1";
+            housing1.Rent = 500;
+
+            housings.Add(housing1);
+
+            ViewBag.CurrentViewName = "List of housings";
+            return View(housings);
         }
     }
 }
